76f64eaf9ac00933670eefe39b306a76083f2f74
Eugene Egorov <eugene.ego@gmail.com>
2019-05-05 13:52:21 +0300

    Updated build script

---
+++ LegacyDemoTests/LegacyDemoTests.swift
@@ -0,0 +1,13 @@
+//
+// LegacyDemoTests
+// LegacyDemoTests
+//
+// Created by Eugene Egorov on 05 May 2019.
+// Copyright (c) 2019 Eugene Egorov. All rights reserved.
+//
+
+import XCTest
+@testable import LegacyDemo
+
+class LegacyDemoTests: XCTestCase {
+}
